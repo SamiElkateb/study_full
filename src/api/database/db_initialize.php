bc93c94e536718f1ae78f680bc0318951ea05335
Sami EL KATEB <sami.elkateb@gmail.com>
2022-01-07 00:20:05 +0100

    changed api database tables

--- src/api/database/db_initialize.php
+++ src/api/database/db_initialize.php
@@ -2,4 +2,6 @@
 require $_SERVER['DOCUMENT_ROOT'] . '/database/tables/cards.php';
 require $_SERVER['DOCUMENT_ROOT'] . '/database/tables/users.php';
 require $_SERVER['DOCUMENT_ROOT'] . '/database/tables/study_data.php';
-require $_SERVER['DOCUMENT_ROOT'] . '/database/tables/navigation.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/database/tables/courses.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/database/tables/chapters.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/database/tables/lessons.php';
